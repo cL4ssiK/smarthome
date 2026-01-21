a2a4b6097ae7d487d08e76035fccc5ce9df59df1
cL4ssiK <lassikupiainen2gmail.com>
2026-01-21 12:43:27 +0200

    functionality to add function forms

--- webserver/frontend/src/modules/DeviceList.jsx
+++ webserver/frontend/src/modules/DeviceList.jsx
@@ -1,21 +1,50 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
 import { DeviceContext } from "../context/DeviceContext";
+//import { DeviceFunctionsForm } from "./deviceFunctionsForm";
 import styles from "./DeviceList.module.css";
 
 function DeviceList() {
     const devicesContext = useContext(DeviceContext);
+    const [deviceFunctions, setDeviceFunctions] = useState([]);
+
+    useEffect(() => {
+        if (!devicesContext.devices) return;
+
+        setDeviceFunctions(
+            devicesContext.devices.map(device => ({ id: device.id, toggled: false }))
+        );
+    }, [devicesContext.devices]);
+
+    function changeDevFuncVisibility(id){
+        setDeviceFunctions(prevDevices => prevDevices.map(device =>
+            device.id == id ? {...device, toggled: !device.toggled} : device
+        ));
+    }
+
+    function handleClick(id){
+        changeDevFuncVisibility(id);
+    }
 
     if (devicesContext.loading) {
         return <p>Loading...</p>;
     }
 
     return (
-        <div>
+        <div >
             {devicesContext.devices.map((device, i) => (
-                <div key={device.id} className={styles.devicecard}>
-                    <h3>{device.name == "" ? "Device " + i : device.name}</h3>
-                    <p>{device.active ? "on" : "off"}</p>
-                </div>
+                <Fragment key={device.id}>
+                    <div id={device.id}
+                        className={styles.devicecard}
+                        onClick={() => handleClick(device.id)}>
+                        <h3>{device.name == "" ? "Device " + i : device.name}</h3>
+                        <p>{device.active ? "on" : "off"}</p>
+                    </div>
+                    <div className={styles.devicecard}>
+                        {deviceFunctions.find(elem => elem.id == device.id)?.toggled &&
+                        <p>tähän devicefunctionformi aina</p>
+                    }
+                    </div>
+                </Fragment>
             ))}
         </div>
     )
