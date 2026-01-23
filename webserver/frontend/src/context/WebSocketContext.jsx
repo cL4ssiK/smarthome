bcce8d920b94789b47ab78897640b84fa7594ae6
cL4ssiK <lassikupiainen2gmail.com>
2026-01-23 20:36:17 +0200

    changed usestate to useref for websocket to get rid of unnecessary reloads

--- webserver/frontend/src/context/WebSocketContext.jsx
+++ webserver/frontend/src/context/WebSocketContext.jsx
@@ -1,50 +1,58 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useRef } from 'react';
 
 export const WebSocketContext = createContext(null);
 
 export function WebSocketProvider({ children }) {
-    const [ws, setWs] = useState(null);
+    const ws = useRef(null);
+    const [wsState, setWsState] = useState("disconnected");
 
     useEffect(() => {
-    let retry;
-    let socket;
-    function connect() {
-      const WS_URL = "ws://127.0.0.1:5000/ws/frontend";
-      const socket = new WebSocket(WS_URL);
+        let retry;
+        let socket;
+        function connect() {
+            const WS_URL = "ws://127.0.0.1:5000/ws/frontend";
+            const socket = new WebSocket(WS_URL);
 
-      socket.onopen = () => {
-        console.log("Connected to server via ws");
-        setWs(socket);
-      }
+            socket.onopen = () => {
+                console.log("Connected to server via ws");
+                ws.current = socket;
+                setWsState("connected");
+            }
 
-      socket.onmessage = msg => console.log("Message from server:", msg.data);
+            socket.onmessage = msg => {
+                console.log("Message from server:", msg.data);
+            }
 
-      socket.onclose = () => {
-        console.log("WS closed, retrying...");
-        if (ws) {
-            setWs(null);
+            socket.onclose = () => {
+                console.log("WS closed, retrying...");
+                if (ws) {
+                    ws.current = null;
+                }
+                setWsState("disconnected");
+                retry = setTimeout(connect, 2000);
+            };
+
+            socket.onerror = () => {
+                if (ws) {
+                    ws.current = null;
+                }
+                socket.close();
+                setWsState("disconnected");
+            };
         }
-        retry = setTimeout(connect, 2000);
-      };
 
-      socket.onerror = () => {
-        if (ws) {
-            setWs(null);
+        connect();
+
+        return () => {
+            clearTimeout(retry);
+            socket?.close();
         }
-        socket.close();
-      };
-    }
+    }, []);
 
-    connect();
 
-    return () => {
-      clearTimeout(retry);
-      socket?.close();
-    }
-  }, []);
 
   return (
-    <WebSocketContext.Provider value={{ws}}>
+    <WebSocketContext.Provider value={{ws, wsState, setWsState}}>
         {children}
     </WebSocketContext.Provider>
   );
