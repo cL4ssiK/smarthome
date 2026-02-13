1023358c1b60847e6aea7fddac8e7623be2b5cce
cL4ssiK <lassikupiainen2gmail.com>
2026-02-13 20:21:27 +0200

    more usable mock device

--- mocks/DeviceMock.js
+++ mocks/DeviceMock.js
@@ -1,9 +1,39 @@
+//const WS_URL = "ws://192.168.100.44:3000/ws/iot";
 const WS_URL = "ws://192.168.100.17:5000/ws/iot";
 const sockets = [];
 
-function newMockDevice() {
+function createFunction(code, name="", initialstate="off", allowtimer=true) {
+    return {
+        code: code,
+        name: name,
+        initialstate: initialstate,
+        allowtimer: allowtimer,
+    };
+}
+
+function functionFactory(caseNro) {
+    let functions = null;
+    switch(caseNro) {
+        case 0:
+            functions = [createFunction(1, "brew coffee")];
+            break;
+        case 1:
+            functions = [
+                createFunction(1, "brew coffee"),
+                createFunction(2, "do nothing", "on", false)
+            ];
+            break;
+        default:
+            functions = [];
+
+    }
+    return functions;
+}
+
+//TODO: Make objects out of these so simulating devices can be better.
+function newMockDevice(funcs) {
     const socket = new WebSocket(WS_URL);
-    const functions = [{ code:1, name: "Ei tee mitään" },{ code:2, name: "Ei tee myöskään mitään" }];
+    const functions = funcs;
     
     socket.onopen = () => {
         console.log("Connected to server via ws");
@@ -20,6 +50,18 @@ function newMockDevice() {
     socket.onmessage = msg => {
         const data = JSON.parse(msg.data);
         console.log("Message from server:", data);
+        if (data.type === "command") {
+            const obj = {
+                type: "functionstate",
+                device_id: "mock_1",
+                func_code: data.payload?.command,
+                payload: {
+                    success: true,
+                    state: "on",
+                }
+            };
+            socket.send(JSON.stringify(obj));
+        } 
     };
 
     socket.onclose = () => {
@@ -29,4 +71,4 @@ function newMockDevice() {
     sockets.push(socket);
 }
 
-newMockDevice();
+newMockDevice(functionFactory(1));
