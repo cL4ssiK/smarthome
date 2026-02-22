191054ca6edcda2ccef4c4710c3bf47a034fce6a
cL4ssiK <lassikupiainen2gmail.com>
2026-02-22 14:54:30 +0200

    device mock is now interactive

--- mocks/DeviceMock.js
+++ mocks/DeviceMock.js
@@ -1,11 +1,13 @@
 //const WS_URL = "ws://192.168.100.44:3000/ws/iot";
 const WS_URL = "ws://192.168.100.17:5000/ws/iot";
 const sockets = [];
+const devices = [];
 
 function createFunction(code, name="", initialstate="off", allowtimer=true) {
     return {
         code: code,
         name: name,
+        state: initialstate,
         initialstate: initialstate,
         allowtimer: allowtimer,
     };
@@ -30,10 +32,21 @@ function functionFactory(caseNro) {
     return functions;
 }
 
+function execute(command) {
+    const state = devices[0].functions[command - 1].state;
+    devices[0].functions[command - 1].state = state === "off" ? "on" : "off";
+    return devices[0].functions[command - 1].state;
+}
+
 //TODO: Make objects out of these so simulating devices can be better.
 function newMockDevice(funcs) {
     const socket = new WebSocket(WS_URL);
     const functions = funcs;
+    const device = {
+        id: "mock_1",
+        type: "coffeemaker",
+        functions: functions,
+    }
     
     socket.onopen = () => {
         console.log("Connected to server via ws");
@@ -52,13 +65,26 @@ function newMockDevice(funcs) {
         const data = JSON.parse(msg.data);
         console.log("Message from server:", data);
         if (data.type === "command") {
+            const state = execute(data.payload?.command);
             const obj = {
                 type: "functionstate",
                 device_id: "mock_1",
                 func_code: data.payload?.command,
                 payload: {
                     success: true,
-                    state: "on",
+                    state: state,
+                }
+            };
+            socket.send(JSON.stringify(obj));
+        }
+        else if (data.type === "state") {
+            const obj = {
+                type: "functionstate",
+                device_id: "mock_1",
+                func_code: data.payload?.command,
+                payload: {
+                    success: true,
+                    state: devices[0].functions[data.payload?.command - 1].state,
                 }
             };
             socket.send(JSON.stringify(obj));
@@ -70,6 +96,7 @@ function newMockDevice(funcs) {
     };
 
     sockets.push(socket);
+    devices.push(device);
 }
 
 newMockDevice(functionFactory(1));
